8b1c9c73da7d782ed79ea99ef6b5777880b45e70
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-25 13:46:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let oli=document.getElementById("infi-list")
-oli.addEventListener('scroll', function() {
-  if (oli.scrollTop + oli.clientHeight >= oli.scrollHeight) {
-    for(let i = 0; i < 2; i++) {
-      let li = document.createElement('li');
-      li.innerText = "New List Item";
-      oli.appendChild(li);
+window.onload=function(){
+let oli=document.getElementById("infi-list");
+	for(let i=0;i<10;i++){
+		let li = document.createElement('li');
+		li.innerText="Initial List Item " + (i + 1);
+		ol.appendChild(li);
+	}
+ oli.addEventListener('scroll', function() {
+    if (oli.scrollTop + oli.clientHeight >= oli.scrollHeight) {
+      for(let i = 0; i < 2; i++) {
+        let li = document.createElement('li');
+        li.innerText = "New List Item";
+        oli.appendChild(li);
+      }
     }
-  }
-});
+  });
+}
