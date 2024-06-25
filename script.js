16ab06bff455977514cb5f08f0febeaf0e3b8f97
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-25 13:48:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let oli=document.getElementById("infi-list");
 	for(let i=0;i<10;i++){
 		let li = document.createElement('li');
 		li.innerText="Initial List Item " + (i + 1);
-		ol.appendChild(li);
+		oli.appendChild(li);
 	}
  oli.addEventListener('scroll', function() {
     if (oli.scrollTop + oli.clientHeight >= oli.scrollHeight) {
